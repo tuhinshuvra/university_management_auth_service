21a7e882171665c9cbefa36d572156a1e5b6b18a
Tuhin <tuhinshuvra18@gmail.com>
2023-11-05 07:19:36 +0600

    [Done] created .env

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+// const port = 3000; 
+
+app.use(cors())
+
+
+// parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to University Management Auth Service!')
+})
+
+export default app;
