0f1a31b7c9fe4d3d1eb890256eeeb005ed300b53
Tuhin <tuhinshuvra18@gmail.com>
2023-11-07 07:22:19 +0600

    added some update

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,23 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import usersRouter from './app/modules/users/users.route'
+
 const app: Application = express();
 // const port = 3000; 
 
 app.use(cors())
 
-
 // parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: any) => {
+
+app.use('/api/v1/users', usersRouter)
+
+app.get('/', async (req: Request, res: Response) => {
     res.send('Welcome to University Management Auth Service!')
 })
+
+
 
 export default app;
---
+++ src/app/modules/users/users.service.ts
@@ -0,0 +1,24 @@
+import config from "../../../config/index";
+import { IUser } from "./users.interface";
+import { User } from "./users.models";
+import { generatedUserId } from "./users.utils";
+
+const createUser = async (user: IUser): Promise<IUser | null> => {
+
+    const id = await generatedUserId()
+    user.id = id
+
+    if (!user.password) {
+        user.password = config.default_user_pass as string
+    }
+
+    const createdUser = await User.create(user)
+
+    if (!createUser) {
+        throw new Error('Failed to create new user!')
+    }
+    return createdUser
+
+}
+
+export default { createUser }
