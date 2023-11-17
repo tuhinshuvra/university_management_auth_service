0de5176bca99149fabafc11aa82a0c59e101eb0b
Tuhin <tuhinshuvra18@gmail.com>
2023-11-17 13:45:37 +0600

    used a common patten for errorHandlaling, tryAsync, response, success and reorganized app.route

--- src/shared/sendResponse.ts
+++ src/shared/sendResponse.ts
@@ -1,12 +1,11 @@
 import { Response } from "express";
 
 
-
 type IApiResponse<T> = {
     statusCode: number;
     success: boolean,
     message?: string | null;
-    data?: T;
+    data?: T | null;
 }
 
 const sendResponse = <T>(res: Response, data: IApiResponse<T>): void => {
